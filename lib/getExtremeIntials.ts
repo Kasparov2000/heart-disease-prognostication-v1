1d917d347999f4a8889255f5df2f4328d7b783a7
Tafara Kasparov Mhangami <79202918+Kasparov2000@users.noreply.github.com>
2024-04-02 00:48:24 +0200

    resolving imports

--- lib/getExtremeIntials.ts
+++
@@ -1,5 +0,0 @@
-export default function getExtremeInitials(name: string) : string {
-    let nameParts = name.split(" ");
-    let initials = nameParts[0].charAt(0) + nameParts[nameParts.length - 1].charAt(0);
-    return initials.toUpperCase();
-}
